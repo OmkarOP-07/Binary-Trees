77b8604e30d37afad96ca03fe914c9a4b315cabd
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2026-02-02 22:18:56 +0530

    done 1st

--- medium/Check_balanced_tree.cpp
+++ medium/Check_balanced_tree.cpp
@@ -1,8 +1,41 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int check_balanced_tree(TreeNode *root)
+{
+    if (root == nullptr)
+        return 0;
+
+    int lh = check_balanced_tree(root->left);
+    int rh = check_balanced_tree(root->right);
+
+    if (lh == -1 && rh == -1)
+        return -1;
+    if (abs(lh - rh) > 1)
+        return -1;
+
+    return max(lh, rh) + 1;
+}
 
 int main()
 {
- return 0;
+    TreeNode *root = new TreeNode(
+        1,
+        new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+        new TreeNode(3));
+
+    int ans = check_balanced_tree(root);
+    cout << ans;
+    return 0;
 }
